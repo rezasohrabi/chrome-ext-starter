1d62d40b6298b8ba21420e5be20f4384ece9a3a4
r.sohrabi <rezasohrabi1995@yahoo.com>
2024-04-01 23:27:59 +0330

    feat: tailwind css

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,39 +2,40 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true, node: true },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react-hooks/recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "prettier",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'prettier',
   ],
-  ignorePatterns: ["dist", ".eslintrc.cjs"],
-  parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "react", "@typescript-eslint", "import"],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react', '@typescript-eslint', 'import'],
   rules: {
-    "react-refresh/only-export-components": [
-      "warn",
+    'react-refresh/only-export-components': [
+      'warn',
       { allowConstantExport: true },
     ],
-    "react/jsx-filename-extension": "off",
-    "import/extensions": "off",
-    "no-unused-vars": "off",
-    "@typescript-eslint/no-unused-vars": ["error"],
-    "no-shadow": "off",
-    "@typescript-eslint/no-shadow": "error",
+    'react/jsx-filename-extension': 'off',
+    'import/extensions': 'off',
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
+    'no-shadow': 'off',
+    '@typescript-eslint/no-shadow': 'error',
+    'import/no-extraneous-dependencies': 'off',
   },
   globals: {
-    chrome: "readonly",
+    chrome: 'readonly',
   },
   settings: {
-    "import/parsers": {
-      "@typescript-eslint/parser": [".ts", ".tsx"],
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
     },
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         alwaysTryTypes: true,
-        project: "./tsconfig.json",
+        project: './tsconfig.json',
       },
     },
   },
