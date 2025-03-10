10f198a3b6375330cf2ffa41c1992ff5f6cfad4c
Burkhard Reffeling <307162+hardchor@users.noreply.github.com>
2025-03-10 21:39:05 +0100

    refactor: tmp remove unused Tailwind CSS configuration and daisyUI plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,4 @@
-import daisyui from 'daisyui';
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{js,jsx,ts,tsx}', './*.html', './src/**/*.html'],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    // Use the imported daisyui plugin
-    daisyui,
-  ],
-  daisyui: {
-    themes: ['light', 'dark'],
-  },
 };
