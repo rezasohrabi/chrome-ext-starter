b6960ea23a893775518f3fd43cf0152d5f8e98fc
Burkhard Reffeling <307162+hardchor@users.noreply.github.com>
2025-04-02 14:10:03 +0200

    feat: remove unused content scripts and update version to 1.1.2

--- src/content/index.prod.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from '@assets/styles/index.css?inline';
-import createShadowRoot from '@utils/createShadowRoot';
-
-import Content from './Content';
-
-const root = createShadowRoot(styles);
-
-root.render(<Content />);
