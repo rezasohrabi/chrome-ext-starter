a52049065b47711e3f5a07090c0ac295af4494bc
r.sohrabi <rezasohrabi1995@yahoo.com>
2025-07-18 11:34:50 +0330

    feat: add touchGlobalCSSPlugin to update global CSS on file changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,39 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable no-param-reassign */
-import { resolve } from 'path';
+import fs from 'fs';
+import path, { resolve } from 'path';
 import { crx } from '@crxjs/vite-plugin';
 import react from '@vitejs/plugin-react';
 import { defineConfig, Plugin } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 import manifest from './src/manifest';
+
+function touchFile(filePath: string): void {
+  const time = new Date();
+  fs.utimesSync(filePath, time, time);
+}
+
+type TouchGlobalCSSPluginOptions = {
+  cssFilePath: string;
+  watchFiles: string[];
+};
+
+export function touchGlobalCSSPlugin({
+  cssFilePath,
+  watchFiles,
+}: TouchGlobalCSSPluginOptions): Plugin {
+  return {
+    name: 'touch-global-css',
+    configureServer(server) {
+      server.watcher.on('change', (file) => {
+        if (watchFiles.some((watchFile) => file.includes(watchFile))) {
+          touchFile(cssFilePath);
+        }
+      });
+    },
+  };
+}
 
 const viteManifestHackIssue846: Plugin & {
   renderCrxManifest: (manifest: any, bundle: any) => void;
@@ -26,6 +53,10 @@ export default defineConfig({
     react(),
     tsconfigPaths(),
     viteManifestHackIssue846,
+    touchGlobalCSSPlugin({
+      cssFilePath: path.resolve(__dirname, 'src/assets/styles/index.css'),
+      watchFiles: ['.tsx'],
+    }),
     crx({
       manifest,
       contentScripts: {
